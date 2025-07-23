adb66dcce618afaf2899b899bbf44204ea5ad154
Phillip Kirk <168415485+kirkphillip605@users.noreply.github.com>
2025-07-23 18:20:29 -0500

    Updated AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -224,7 +224,7 @@ export const AuthProvider = ({ children }) => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`
+        redirectTo: `${window.location.origin}/`
       }
     });
     if (error) throw new Error(error.message);
