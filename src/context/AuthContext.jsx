40111b59428119d69a2c073df4d120e0690de448
Phillip Kirk <168415485+kirkphillip605@users.noreply.github.com>
2025-07-22 19:01:00 -0500

    Updated AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -145,7 +145,7 @@ export const AuthProvider = ({ children }) => {
     
     if (supabase) {
       const { data: authSubscription } = supabase.auth.onAuthStateChange(
-      (event, session) => {
+      await (event, session) => {
         if (!mounted) return;
 
         console.log('Auth state change:', event, session?.user?.id);
