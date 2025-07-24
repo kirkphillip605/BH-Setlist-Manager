3b2739c639149daa436f5b1a91ec0498ed1aa1c1
Phillip Kirk <168415485+kirkphillip605@users.noreply.github.com>
2025-07-24 00:25:22 -0500

    Updated pdfGenerator.js

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -74,7 +74,7 @@ export const generateSetlistPDF = async (setlist) => {
 
         // Performance note as superscript on same line
         if (song.performance_note) {
-          const note = `--${song.performance_note}`;
+          const note = `  |${song.performance_note}|`;
           const titleWidth = pdf.getTextWidth(song.title) + 4;
           pdf.setFontSize(SUPERSCRIPT_SIZE);
           pdf.text(note, margin + titleWidth, cursorY - SUPERSCRIPT_OFFSET);
@@ -90,7 +90,7 @@ export const generateSetlistPDF = async (setlist) => {
 
         let metaText = song.original_artist || '';
         if (song.key_signature) {
-          metaText += ` - ${song.key_signature}`;
+          metaText += ` | Key: ${song.key_signature}`;
         }
         pdf.text(metaText, margin, cursorY);
         cursorY += SONG_META_SIZE + 8;
